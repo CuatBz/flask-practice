a95be5d803b1aed3fdaa2e0af4eb7f90c76e7c87
Nathanael Moh <50465141+CuatBz@users.noreply.github.com>
2026-09-16 12:44:44 -0600

    Add pagination changes to backend

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -40,10 +40,12 @@ def edit_profile():
 @app.route("/explore")
 @login_required
 def explore():
+    page = request.args.get("page", 1, type=int)
     query = sa.select(Post).order_by(Post.timestamp.desc())
-    posts = db.session.scalars(query).all()
+    posts = db.paginate(query, page=page,
+                        per_page=app.config["POSTS_PER_PAGE"], error_out=False)
 
-    return render_template("index.html", title="Explore", posts=posts)
+    return render_template("index.html", title="Explore", posts=posts.items)
 
 
 @app.route("/follow/<username>", methods=["POST"])
@@ -87,10 +89,13 @@ def index():
         flash("Your post is now live!")
         return redirect(url_for("index"))
 
-    posts = db.session.scalars(current_user.following_posts()).all()
+    page = request.args.get("page", 1, type=int)
+
+    posts = db.paginate(current_user.following_posts(), page=page,
+                        per_page=app.config["POSTS_PER_PAGE"], error_out=False)
 
     return render_template("index.html", title="Home Page", form=form,
-                           posts=posts)
+                           posts=posts.items)
 
 
 @app.route("/login", methods=["GET", "POST"])
