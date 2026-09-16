7e5aaec2b7d9583091211742319b93e1f81b918e
Nathanael Moh <50465141+CuatBz@users.noreply.github.com>
2026-09-16 13:12:42 -0600

    Add real posts to user and pagination

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -196,11 +196,18 @@ def unfollow(username):
 def user(username):
     user = db.first_or_404(sa.select(User).where(User.username == username))
 
-    posts = [
-        {"author": user, "body": "Test post #1"},
-        {"author": user, "body": "Test post #2"}
-    ]
+    page = request.args.get("page", 1, type=int)
+    query = user.posts.select().order_by(Post.timestamp.desc())
+    posts = db.paginate(query, page=page,
+                        per_page=app.config["POSTS_PER_PAGE"],
+                        error_out=False)
+
+    next_url = url_for("user", username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for("user", username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
 
     form = EmptyForm()
 
-    return render_template("user.html", form=form, user=user, posts=posts)
+    return render_template("user.html", form=form, user=user, posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
--- microblog/config.py
+++ microblog/config.py
@@ -14,4 +14,4 @@ class Config:
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     ADMINS = ["moh.nathanael@protonmail.com"]
 
-    POSTS_PER_PAGE = 3
+    POSTS_PER_PAGE = 10
